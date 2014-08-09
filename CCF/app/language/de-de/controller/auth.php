97e5ae119ba8cd73e4cd1e9c088a7517483c4f96
Mario Döring <deluna.yy@gmail.com>
2014-08-09 16:49:22 +0200

    Fixed some german translations

--- CCF/app/language/de-de/controller/auth.php
+++ CCF/app/language/de-de/controller/auth.php
@@ -5,4 +5,6 @@
 	
 	'sign_up.topic'	=> 'Registrieren',
 	'sign_up.submit'	=> 'Konto Erstellen',
+	'sign_up.message.email_in_use' => 'Diese E-Mail Adresse wird bereits verwendet.',
+	'sign_up.message.success' => 'Ihr Konto wurde erfolgreich erstellt.',
 );
